0121c65339ec82b90f4316fdcacadade7cd23f85
JF Pambrun <jf.pambrun@gmail.com>
2025-12-30 11:14:10 -0500

    wip parquet packing

---
+++ example.sql
@@ -0,0 +1,34 @@
+-- select "00080018" from read_vortex('*/*.vortex');
+
+
+-- select "00080018" from '*/*.parquet';
+
+-- select "00080028"->'$.Value[0]' from '*/*.parquet';
+
+
+
+
+-- select "0020000D","00080018", "00100010", "" from '*/*.parquet';
+
+
+CREATE OR REPLACE VIEW instances AS
+SELECT
+  TRY("00020010"->>'$.Value[0]')::VARCHAR as TransferSyntaxUID,
+  TRY("00080018"->>'$.Value[0]')::VARCHAR as SOPInstanceUID,
+  TRY("00080032"->>'$.Value')::VARCHAR as AcquisitionTime,
+  -- TRY("00200032"->>'$.Value[0]')::VARCHAR as ImagePositionPatient,
+  -- TRY("00200032") as ImagePositionPatient,
+  TRY(strptime("00080020"->>'$.Value[0]', '%Y%m%d'))::Date AS StudyDate,
+  TRY("00080060"->>'$.Value[0]')::VARCHAR as Modality,
+  privateTags->>'$.00090001.Value[0]' as FileName
+FROM '*/*.parquet';
+
+SELECT * from instances ORDER BY StudyDate;
+
+SELECT Modality, count(*) as cnt
+FROM instances
+GROUP BY Modality
+ORDER by cnt DESC;
+
+
+SELECT TRY(''->>'$.Value[0]')::VARCHAR;
---
+++ pack.mjs
@@ -0,0 +1,81 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-env --allow-ffi
+import { mkdir } from "node:fs/promises";
+import { open } from "npm:lmdb";
+
+const db = open({ path: "_dicomweb", useVersions: true });
+
+import { DuckDBInstance } from "npm:@duckdb/node-api";
+
+const instance = await DuckDBInstance.create(":memory:");
+const connection = await instance.connect();
+await connection.run("INSTALL vortex;LOAD vortex;");
+
+await mkdir("_vortex", { recursive: true });
+await mkdir("_parquet", { recursive: true });
+
+function isPrivateTag(tagString) {
+	const groupHex = tagString.substring(0, 4);
+	const group = parseInt(groupHex, 16);
+	return group % 2 === 1;
+}
+
+const baseTags = new Set([
+  "00200032",
+  "00080032",
+  ]);
+
+for (const studyKey of db.getKeys({
+	start: "study:",
+	end: "study:" + "\ufffd",
+})) {
+	const studyInstanceUID = studyKey.split(":")[1];
+	const instances = [
+		...db
+			.getRange({
+				start: `instance:${studyInstanceUID}:`,
+				end: `instance:${studyInstanceUID}:\ufffd`,
+			})
+			.map(({ value }) => value),
+	];
+
+	// maybe sort by series uid?
+
+	const topLevelTagsSet = new Set();
+	// const tagVrMap = new Map();
+	for (const instance of instances) {
+		for (const tag of Object.keys(instance)) {
+			topLevelTagsSet.add(tag);
+			// tagVrMap.set(tag, instance[tag].vr);
+		}
+	}
+
+	const privateTags = Array.from(topLevelTagsSet).filter((tag) =>
+		isPrivateTag(tag),
+	);
+	const publicTags = Array.from(baseTags.union(topLevelTagsSet)).filter(
+		(tag) => !isPrivateTag(tag),
+	);
+	const publicTagList = publicTags.map((t) => `"${t}" varchar`).join(",");
+	await connection.run(
+		`create or replace table temp_instance_table(${publicTagList}, privateTags varchar)`,
+	);
+	const appender = await connection.createAppender('temp_instance_table');
+	for (const instance of instances) {
+	  for (const publicTag of publicTags){
+			appender.appendVarchar(instance[publicTag] ? JSON.stringify(instance[publicTag]) : '');
+		}
+
+		const privateTagObject = {};
+		for (const privateTag of privateTags) {
+      if (instance[privateTag]) {
+        privateTagObject[privateTag] = instance[privateTag];
+      }
+    }
+		appender.appendVarchar(JSON.stringify(privateTagObject))
+		appender.endRow();
+	}
+	appender.closeSync();
+	// await connection.run(`COPY (FROM temp_instance_table) TO '_vortex/${studyInstanceUID}.vortex' (FORMAT vortex);`);
+	await connection.run(`COPY (FROM temp_instance_table) TO '_parquet/${studyInstanceUID}.parquet' (FORMAT parquet);`);
+
+}
